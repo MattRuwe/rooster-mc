770c25abd8c1c2d3c6fa481748e43cc227d6570c
Matt Ruwe <matthew.ruwe@kiewit.com>
2022-12-23 09:31:08 -0600

    Cleaned up commented code

--- src/main/java/roostermc/roostermc/handlers/PlayerKilledHandler.java
+++ src/main/java/roostermc/roostermc/handlers/PlayerKilledHandler.java
@@ -4,12 +4,9 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import roostermc.roostermc.RoosterMC;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class PlayerKilledHandler implements Listener {
     public PlayerKilledHandler(RoosterMC plugin) {
@@ -18,16 +15,6 @@ public class PlayerKilledHandler implements Listener {
 
     @EventHandler
     public void onPlayerDeath_Normal(PlayerDeathEvent event){
-//        String deathMessage = event.getDeathMessage();
-//        Pattern pattern = Pattern.compile("(?<deadPlayer>\\w+) was slain by (?<killer>\\w+)");
-//        Matcher matcher = pattern.matcher(deathMessage);
-//        if(matcher.find()) {
-//            String deadPlayer = matcher.group("deadPlayer");
-//            Bukkit.getLogger().info("deadPlayer = " + deadPlayer);
-//            String killer = matcher.group("killer");
-//            Bukkit.getLogger().info("killer = " + killer);
-//            Player killerPlayer = Bukkit.getPlayer(killer);
-//            Bukkit.getLogger().info("killerPlayer = " + killerPlayer.getName());
             String deadPlayerName = event.getEntity().getName();
             Player killerPlayer = event.getEntity().getKiller();
             if (deadPlayerName.equals("majodio") && killerPlayer != null && !killerPlayer.getName().equals("majodio")) {
@@ -35,7 +22,5 @@ public class PlayerKilledHandler implements Listener {
                 event.setDeathMessage(deadPlayerName + " was slain, but immediately got revenge on " + killerPlayer.getName());
                 killerPlayer.setHealth(0);
             }
-//        }
-        //Bukkit.getLogger().info("Player Name = " + );
     }
 }
